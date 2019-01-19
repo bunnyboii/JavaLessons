d450aa72ad02483b499a31546b8aa8c32926ca3d
Saul <saulquintanar1738@gmail.com>
2019-01-19 14:50:31 -0800

    Uploading code

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)  {
+        int[] myarrray = new int[5];
+        for(int y = 2; y <= 10; y=y+2){
+            myarray[0] = y;
+            System.out.println(y);
+        }
+                    
+    }
+}
---
+++ test.java
@@ -0,0 +1,13 @@
+public class test{
+    public static void main(String[] args){
+        int[] myarray = new int[5];
+        for(int y=2; y<=10; y=y+2){
+            myarray[((y-2)/2)] = y;
+            System.out.println(y);
+        }
+
+        for(int i  = 0; i <5; i++){
+            System.out.println(myarray[i]);
+        }
+    }
+}
