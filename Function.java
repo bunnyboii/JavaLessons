3667c33a58bfab303352b3c6a7a43df9b9bc1315
Saul <saulquintanar1738@gmail.com>
2019-01-19 19:20:03 -0800

    Checks if any three numbers are even

--- Function.java
+++ Function.java
@@ -1,11 +1,13 @@
 import com.sun.net.httpserver.Authenticator.Result;
 
-public class Function {
+public class Function { 
     public static void main(String[] arg){
 
      System.out.println(evenNumbers(4, 10, 8));
 
     }
+
+    // This function checks if any three numbers are even
    public static boolean evenNumbers(int num1, int num2, int num3){
     boolean result = num1%2==0 && num2%2==0 && num3%2==0;  
     
